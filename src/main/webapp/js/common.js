edfe397b2814ee31e863a67eee0ab1af33c2e939
ynjch97 <ynjch2017@naver.com>
2021-05-13 02:03:21 +0900

    cookie 관련, 페이지 이동 관련 script function 추가 (common.js)

--- src/main/webapp/js/common.js
+++ src/main/webapp/js/common.js
@@ -1,3 +1,30 @@
+/*
+* 페이지 이벤트 관련
+*/ 
+
+// 페이지 이동
+function movePage(urlStr, param) {
+	$("#_tempForm").remove();
+	var sHtml = '';
+	sHtml += '<form id="_tempForm" action="' + urlStr + '" method="post">';
+	sHtml += '</form>';
+	$("body").append(sHtml);
+	
+	if (param != null) {
+		var keys = Object.keys(param);
+		for (var i=0; i<keys.length; i++) {
+			var key = keys[i];
+			$("#_tempForm").append('<input type="hidden" name="' + key + '"/>');
+			$("#_tempForm input:last").val(param[key]);
+		}
+	}
+	$("#_tempForm").submit();
+}
+
+/*
+* 입력값 관련
+*/ 
+
 // 빈값 체크
 function isEmpty(str) {
 	var chkVal = true;
@@ -24,4 +51,89 @@ function compareStr(str1, str2) {
 		chkVal = true;
 	}
 	return chkVal;
+}
+
+// 메일 형식 체크
+function isValidEmail(str) {
+	return /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i.test(str);
+}
+
+// 비밀번호 형식 체크
+function isValidPassword(str) {
+	return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[`,!,”,￡,$,%,^,&,*,(,),_,+,\-,=,{,},\[,\],:,@,~,;,’,#,<,>,?,.,\/,\,|])[0-9a-zA-Z`,!,”,￡,$,%,^,&,*,(,),_,+,\-,=,{,},\[,\],:,@,~,;,’,#,<,>,?,.,\/,\,|]{8,16}$/.test(str);
+}
+
+// 숫자 형식 체크
+function isValidNumber(str) {
+	return /^[0-9]+$/.test(str);
+}
+
+// url 유효성 검사
+function isValidUrl(str) {
+	var pattern = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
+	if (pattern.test(str)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/*
+* 쿠키 관련
+*/ 
+
+//쿠키값 추출
+function getCookie(cookieName) { // 쿠키명
+	var cookie = document.cookie;
+	
+	// 현재 쿠키가 존재할 경우
+	if (cookie.length > 0) {
+		// 해당 쿠키명이 존재하는지 검색한 후 존재하면 위치를 리턴.
+		startIndex = cookie.indexOf(cookieName);
+	
+		// 만약 존재한다면
+	  	if (startIndex != -1) {
+			// 값을 얻어내기 위해 시작 인덱스 조절
+		 	startIndex += cookieName.length;
+	
+		 	// 값을 얻어내기 위해 종료 인덱스 추출
+		 	endIndex = cookie.indexOf(";", startIndex);
+	
+		 	// 만약 종료 인덱스를 못찾게 되면 쿠키 전체길이로 설정
+		 	if(endIndex == -1) endIndex = cookie.length;
+	
+		 	// 쿠키값을 추출하여 리턴
+		 	return unescape(cookie.substring(startIndex + 1, endIndex));
+	  	}
+	  	else {
+	  		// 쿠키 내에 해당 쿠키가 존재하지 않을 경우
+			return false;
+		}
+	}
+	else {
+		// 쿠키 자체가 없을 경우
+	 	return false;
+	}
+}
+
+// 쿠키 설정
+function setCookie(cookieName, cookieValue, expireDate) { // 설정할 쿠키명, 쿠키 값, 쿠키 유효날짜
+	 var today = new Date();
+	 today.setDate( today.getDate() + parseInt(expireDate) );
+	 document.cookie = cookieName + "=" + escape( cookieValue ) + "; path=/; expires=" + today.toGMTString() + ";";
+}
+
+// 쿠키 삭제
+function deleteCookie(cookieName) { // 삭제할 쿠키명
+	var expireDate = new Date();
+ 
+	// 어제 날짜를 쿠키 소멸 날짜로 설정
+	expireDate.setDate( expireDate.getDate() - 1 );
+	document.cookie = cookieName + "= " + "; expires=" + expireDate.toGMTString() + "; path=/";
+}
+
+// 전체 쿠키 출력
+function viewCookie() {
+	if( document.cookie.length > 0 )
+		console.log("viewCookie : " + document.cookie);
 }
