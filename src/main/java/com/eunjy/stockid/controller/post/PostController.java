b1b06f82497da291cc9cef7e999b8f95de3fad63
ynjch97 <ynjch2017@naver.com>
2021-05-20 00:40:09 +0900

    포스트 목록 > url 의 파라미터값으로 투자 유형 구분

--- src/main/java/com/eunjy/stockid/controller/post/PostController.java
+++ src/main/java/com/eunjy/stockid/controller/post/PostController.java
@@ -2,8 +2,11 @@ package com.eunjy.stockid.controller.post;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.eunjy.stockid.utiliy.Consts;
 
 
 /**
@@ -13,8 +16,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class PostController {
 
-	@RequestMapping(value = "/post/list.do", method = RequestMethod.GET) 
-	public String postList(Model model) { 
+	@RequestMapping(value = "/{investType}/list.do", method = RequestMethod.GET) 
+	public String postList(@PathVariable("investType") String investType, Model model) {
+		String investTypeVal = "00"; // CMMN_CD_INFO 의 INVEST_TYPE(투자 유형) 그룹 값 (01:주식/02:코인) 
+		if (Consts.InvestType.INVEST_STOCK.equals(investType)) {
+			investTypeVal = "01";
+		} else if (Consts.InvestType.INVEST_COIN.equals(investType)) {
+			investTypeVal = "02";
+		} else {
+			return "";
+		}
+		System.out.println("investType : " + investType);
+		System.out.println("investTypeVal : " + investTypeVal);
+		
 		return "post/postList"; 
 	}
 	
--- src/main/java/com/eunjy/stockid/utiliy/Consts.java
+++ src/main/java/com/eunjy/stockid/utiliy/Consts.java
@@ -14,6 +14,26 @@ package com.eunjy.stockid.utiliy;
  * </PRE>
  */
 public class Consts {
+	
+	/**
+	 * 투자 유형 정보
+	 *
+	 * <PRE>
+	 * 1. ClassName: InvestType
+	 * 2. FileName : Consts.java
+	 * 3. 작성자   : ynjch97
+	 * 4. 작성일   : 2021.5.20.
+	 * 5. 변경이력
+	 *		이름			일자				변경내용
+	 *     ———————————————————————————————————————————
+	 *		ynjch97		2021.5.20.		신규 개발
+	 * </PRE>
+	 */
+	public abstract static class InvestType {
+		/** 투자 유형 정보 */
+		public static final String INVEST_COIN = "coin";
+		public static final String INVEST_STOCK = "stock";
+	}
 
 	/**
 	 * 세션 정보
