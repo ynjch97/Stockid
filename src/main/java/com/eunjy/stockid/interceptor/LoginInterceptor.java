7fb465465a66cff5de3d2d62014e7f591852a1cd
ynjch97 <ynjch2017@naver.com>
2021-06-09 17:25:59 +0900

    interceptor 세션 확인

--- src/main/java/com/eunjy/stockid/interceptor/LoginInterceptor.java
+++ src/main/java/com/eunjy/stockid/interceptor/LoginInterceptor.java
@@ -2,12 +2,14 @@ package com.eunjy.stockid.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.eunjy.stockid.domain.common.SessionUser;
+import com.eunjy.stockid.utiliy.Consts;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -25,6 +27,16 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 		
 		log.debug("==================== BEGIN ====================");
 		log.debug("Request URI ===> " + request.getRequestURI());
+
+		HttpSession httpSession = request.getSession();
+		SessionUser sessionUser = (SessionUser) httpSession.getAttribute(Consts.SessionAttr.USER);
+		
+		if (sessionUser == null) {
+			log.debug("로그인 되어있지 않음");
+		} else {
+			log.debug("사용자 아이디 ===> {}", sessionUser.getUsrId());
+		}
+		
 		log.debug("===============================================");
 		
 		return super.preHandle(request, response, handler);
