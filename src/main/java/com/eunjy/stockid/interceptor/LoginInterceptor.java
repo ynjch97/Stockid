fa5d0145674d3ce83e4276318992bfa0c9ce215d
ynjch97 <ynjch2017@naver.com>
2021-06-22 01:16:36 +0900

    interceptor 세션 체크 예외 페이지 설정 (추후 보완 필요 > yml 파일에 예외 페이지 경로값 세팅)

--- src/main/java/com/eunjy/stockid/interceptor/LoginInterceptor.java
+++ src/main/java/com/eunjy/stockid/interceptor/LoginInterceptor.java
@@ -1,5 +1,8 @@
 package com.eunjy.stockid.interceptor;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -21,22 +24,75 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class LoginInterceptor extends HandlerInterceptorAdapter {
 
+	private String redirectPage = "";
+	// private List<Map<String, String>> tests = Consts.NoSession.tests; 
+	private List<String> noSession;
+	private List<String> noSessionPattern;
+	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		
 		log.debug("==================== BEGIN ====================");
 		log.debug("Request URI ===> " + request.getRequestURI());
+		// log.debug("redirectPage ===> " + tests.get(0).get("name"));
+		
+		// uri 주소 정보
+		String requestUri = request.getRequestURI();
+		String[] requestArr = requestUri.split("/");
 
+		// 세선 사용자 정보
+		String usrId = "";
 		HttpSession httpSession = request.getSession();
 		SessionUser sessionUser = (SessionUser) httpSession.getAttribute(Consts.SessionAttr.USER);
 		
-		if (sessionUser == null) {
-			log.debug("로그인 되어있지 않음");
+		log.debug("sessionUser : {}", sessionUser);
+		
+		// login check
+		if ( !isChkSession(requestUri) ) {
+			log.debug("세션 체크 페이지 아님");
+			return super.preHandle(request, response, handler);
 		} else {
-			log.debug("사용자 아이디 ===> {}", sessionUser.getUsrId());
+			if (sessionUser == null) {
+				log.debug("로그인 되어있지 않음");
+				response.sendRedirect(redirectPage);
+				return false;
+			}
+			
+			usrId = sessionUser.getUsrId();
+			log.debug("사용자 아이디 ===> {}", usrId);
+		}
+		// login check end
+		
+		// TODO
+		/*
+		Menu menu = new Menu();
+		menu.setUrl(requestUri);
+		menu.setUsr_grp_id(usr_grp_id);
+		menu.setUsr_id(usr_id);
+		
+		if (requestArr[1].equals("admin")) {
+			if (menuService.countMenuAuth(menu) == 0) {
+		    	// 권한 체크하여 권한이 없으면  permission denied 페이지로
+				request.setAttribute("errCode", "SERVICE_DENIED");
+				response.sendRedirect(redirectPermissionDeniedPage);
+				return false;
+			} 
 		}
 		
+		List<Menu> menuList = menuService.getUserGroupMenuList(usr_grp_id, usr_id);
+
+		request.setAttribute("userGroupMenuList", menuList);
+		request.setAttribute("language", MessageUtil.getMessage("label.common.language"));
+		// get menu end
+
+		// menu activation start
+		this.activateMenu(request, menuList, requestUri);
+		// menu activation end
+		
+		logger.debug("55555");
+		*/
+
 		log.debug("===============================================");
 		
 		return super.preHandle(request, response, handler);
@@ -50,6 +106,26 @@ public class LoginInterceptor extends HandlerInterceptorAdapter {
 		log.debug("===============================================");
 		
 		super.postHandle(request, response, handler, modelAndView);
+	}
+	
+	private boolean isChkSession(String reqURI) {
+		// TODO
+		// String[] requestArr = reqURI.split("/");
+		
+		if ( noSession.contains(reqURI) ) { // 세션 체크 예외 페이지
+			return false;
+		}
+		
+		for ( String pathPattern : noSessionPattern ) { // 세션 체크 예외 페이지 패턴
+			if ( reqURI.indexOf( pathPattern.substring(0, pathPattern.indexOf("/*")) ) == 0 ) {
+				return false;
+			}
+			
+			/*if (StringUtils.countMatches(reqURI, "/") != StringUtils.countMatches(pathPattern, "/")) {
+				continue;
+			}*/
+		}
+		return true;
 	}
 
 }
